b833b38fdd7c50590775900f5ae262f902b3e3a1
Emanuel Coletti <emanuelcoletti001@gmail.com>
2025-05-28 20:00:08 -0300

    Inicializacion app Flask con configuración por variables de entorno y ruta de prueba

---
+++ todo/__init__.py
@@ -0,0 +1,20 @@
+import os # Importamos "os" que nos sirve para acceder a ciertas cosas del sistema operativo, en este caso (Variables de entorno)
+
+from flask import Flask         #
+
+def create_app():               #   Usamos esta funcion para crear instancias direfentes de la misma
+    app = Flask(__name__)       #   Todas las aplicaciones que creamos en Flask son una instancia de Flask
+
+    app.config.from_mapping(
+        SECRET_KEY='mikey',
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+    )
+
+    @app.route('/hola')
+    def hola():
+        return 'Ema feliz'
+    
+    return app
